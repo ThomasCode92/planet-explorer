593201a8223b171ab7eec1e3c8b8932d55e33789
ThomasCode92 <thomascode92@hotmail.com>
2023-11-26 15:22:01 +0100

    Reading the Planet Data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
 const { parse } = require('csv-parse');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .on('data', data => {
+    results.push(data);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  })
+  .on('error', error => {
+    console.error(error);
+  });
