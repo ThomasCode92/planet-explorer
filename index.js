20cb9dd224f075cee5e23bbdfc5268e3ce757298
ThomasCode92 <thomascode92@hotmail.com>
2023-11-26 15:22:11 +0100

    Parsing the Planets Data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { parse } = require('csv-parse');
 const results = [];
 
 fs.createReadStream('kepler_data.csv')
+  .pipe(parse({ comment: '#', columns: true }))
   .on('data', data => {
     results.push(data);
   })
