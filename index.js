f2c8340dc917a1efbb36195bf6867b8e9735dbf5
ThomasCode92 <thomascode92@hotmail.com>
2023-11-26 15:33:02 +0100

    Finding Habitable Planets

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({ comment: '#', columns: true }))
   .on('data', data => {
-    results.push(data);
+    if (!isHabitablePlanet(data)) return;
+    habitablePlanets.push(data);
   })
   .on('end', () => {
-    console.log(results);
-    console.log('done');
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   })
   .on('error', error => {
     console.error(error);
